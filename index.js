afc77c550ae77fff71e56d36b97b5e46dd1993f8
Zubarev Sergey <barclays13@mail.ru>
2019-09-12 23:03:36 +0300

    1 комит дз 12

---
+++ index.js
@@ -0,0 +1,75 @@
+
+window.addEventListener('DOMContentLoaded', function(){
+    'use sctrict';
+
+
+    function countTimer(deadline){
+
+            let timerHours = document.querySelector('#timer-hours'),
+                timerMinutes = document.querySelector('#timer-minutes'),
+                timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining(){
+            let dateStop = new Date (deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = ( dateStop - dateNow ) / 1000,
+            seconds = Math.floor( timeRemaining % 60),
+            minutes = Math.floor ( (timeRemaining / 60) % 60 ),
+            hours = Math.floor( timeRemaining / 60 / 60) % 24;
+
+            return {timeRemaining, hours, minutes, seconds};
+
+        }
+
+
+        function updateClock(){
+            let timer = getTimeRemaining();
+
+                if(timer.hours >= 10){
+
+                    timerHours.textContent = timer.hours;
+                } else {
+                    timerHours.textContent = '0' + timer.hours;
+                }
+
+
+                if( timer.minutes >= 10 ){
+                    
+                    timerMinutes.textContent = timer.minutes;
+
+                } else {
+
+                    timerMinutes.textContent = '0' + timer.minutes;
+                }
+
+
+                if(  timer.seconds >= 10){
+
+                    timerSeconds.textContent = timer.seconds;
+                } else {
+                    timerSeconds.textContent = '0' + timer.seconds;
+                }
+
+
+                if (timer.timeRemaining > 0){
+                    
+                    setInterval (updateClock, 1000);
+                } else {
+                    timerHours.textContent = "00";
+                    timerHours.style.color = 'red';
+                    timerMinutes.textContent = "00";
+                    timerMinutes.style.color = 'red';
+                    timerSeconds.textContent = "00";
+                    timerSeconds.style.color = 'red';
+                }
+
+
+        }
+
+        updateClock();
+
+
+    }
+
+    countTimer ('15 September 2019');
+});
