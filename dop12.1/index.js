5a8bcda9b00cd5fcf808ffed9cec2619bd766e5e
Andrey <xwinged1@gmail.com>
2019-09-14 15:41:38 +0300

    '12дз'

--- dop12.1/index.js
+++ dop12.1/index.js
@@ -1,63 +1,66 @@
 'use strict';
-let data = new Date ();
-let text = document.querySelector('.text');
 
-function dayPeriod (){
-    if( data.getHours >= 0 && data.getHours < 4 ){
-        return "Доброй ночи";
-    } if( data.getHours >= 4 && data.getHours < 9 ){
-        return "Доброе утро";
-    } if( data.getHours >= 9 && data.getHours < 15 ){
-        return "Добрый день";
-    } if( data.getHours >= 15 && data.getHours < 22 ){
-        return "Добрый вечер";
-    } else {
-        return "Доброй ночи";
-    }
+
+
+let showDate = function (){
+
+        let date = new Date ();
+        let text = document.querySelector('.text');
+
+        function dayPeriod (){
+
+            if ( date.getHours() >= 4 && date.getHours() < 9 ){
+                return "Доброе утро";
+            } if ( date.getHours() >= 9 && date.getHours() < 15 ){
+                return "Добрый день";
+            } if ( date.getHours() >= 15 && date.getHours() < 22 ){
+                return "Добрый вечер";
+            } else {
+                return "Доброй ночи";
+            }
+        }
+
+        function weekday() {
+
+            switch (date.getDay()) {
+                case 0:
+                    return "Воскресенье";
+                    break;
+                case 1:
+                    return "Понедельник";
+                    break;
+                case 2:
+                    return "Вторник";
+                    break;
+                case 3:
+                    return "Среда";
+                    break;
+                case 4:
+                    return "Четверг";
+                    break;
+                case 5:
+                    return "Пятница";
+                    break;
+                case 6:
+                    return "Суббота";
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        function Days(){
+                return  Math.floor(( Date.parse('1 January 2020') - Date.now() ) / 86400000);
+        }
+
+        text.innerText = ` 
+        ${dayPeriod ()}         Сегодня: ${weekday()} 
+        Текущее время:${date.toLocaleTimeString()}  
+        До нового года осталось ${Days()} дн.`
+
 }
 
 
 
-function weekday() {
-
-    switch (data.getDay()) {
-        case 0:
-            return "Воскресенье";
-            break;
-        case 1:
-            return "Понедельник";
-            break;
-        case 2:
-            return "Вторник";
-            break;
-        case 3:
-            return "Среда";
-            break;
-        case 4:
-            return "Четверг";
-            break;
-        case 5:
-            return "Пятница";
-            break;
-        case 6:
-            return "Суббота";
-            break;
-
-        default:
-            break;
-    }
-}
-
-
-function Days(){
-        return  Math.floor(( Date.parse('1 January 2020') - Date.now() ) / 86400000);
-}
-
-text.textContent = ` 
-${dayPeriod ()}
-Сегодня: ${weekday()}
-Текущее время:${data.toLocaleTimeString('en')}  
-До нового года осталось ${Days()} дн.`
-
-
-
+setInterval( showDate , 1000);
--- index.js
+++ index.js
@@ -1,70 +1,61 @@
 
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function () {
     'use sctrict';
 
     //Timer
 
-    function countTimer(deadline){
+    function countTimer(deadline) {
 
-            let timerHours = document.querySelector('#timer-hours'),
-                timerMinutes = document.querySelector('#timer-minutes'),
-                timerSeconds = document.querySelector('#timer-seconds'),
-                timerNumbers = document.querySelector('.timer-numbers');
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds'),
+            timerNumbers = document.querySelector('.timer-numbers');
 
-        function getTimeRemaining(){
-            let dateStop = new Date (deadline).getTime(),
-            dateNow = new Date().getTime(),
-            timeRemaining = ( dateStop - dateNow ) / 1000,
-            seconds = Math.floor( timeRemaining % 60),
-            minutes = Math.floor ( (timeRemaining / 60) % 60 ),
-            hours = Math.floor( timeRemaining / 60 / 60) % 24;
+        function getTimeRemaining() {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60) % 24;
 
-            return {timeRemaining, hours, minutes, seconds};
+            return { timeRemaining, hours, minutes, seconds };
 
         }
 
 
-        function updateClock(){
+        function updateClock() {
 
-                let timer = getTimeRemaining();
+            let timer = getTimeRemaining();
 
-                if(timer.hours >= 10){
+            if (timer.hours >= 10) {
+                timerHours.textContent = timer.hours;
+            } else {
+                timerHours.textContent = '0' + timer.hours;
+            }
 
-                    timerHours.textContent = timer.hours;
-                } else {
-                    timerHours.textContent = '0' + timer.hours;
-                }
+            if (timer.minutes >= 10) {
+                timerMinutes.textContent = timer.minutes;
+            } else {
+                timerMinutes.textContent = '0' + timer.minutes;
+            }
 
-                if( timer.minutes >= 10 ){
-                    
-                    timerMinutes.textContent = timer.minutes;
-
-                } else {
-
-                    timerMinutes.textContent = '0' + timer.minutes;
-                }
-
-                if(  timer.seconds >= 10){
-
-                    timerSeconds.textContent = timer.seconds;
-                } else {
-                    timerSeconds.textContent = '0' + timer.seconds;
-                }
+            if (timer.seconds >= 10) {
+                timerSeconds.textContent = timer.seconds;
+            } else {
+                timerSeconds.textContent = '0' + timer.seconds;
+            }
 
 
-                // проверка на отрицательное время
-                if (timer.timeRemaining > 0){
-                    
-                    setInterval (updateClock, 1000);
-
-                } else {
-
-                    timerHours.textContent = "00";
-                    timerMinutes.textContent = "00";
-                    timerSeconds.textContent = "00";
-                    timerNumbers.style.color = 'red';
-
-                }
+            // проверка на отрицательное время
+            if (timer.timeRemaining > 0) {
+                setInterval(updateClock, 1000);
+            } else {
+                timerHours.textContent = "00";
+                timerMinutes.textContent = "00";
+                timerSeconds.textContent = "00";
+                timerNumbers.style.color = 'red';
+            }
 
         }
 
@@ -73,87 +64,87 @@ window.addEventListener('DOMContentLoaded', function(){
 
     }
 
-    countTimer ('15 September 2019');
+    countTimer('12 September 2019');
 
     //Menu
 
-    const toggleMenu = ()=> {
+    const toggleMenu = () => {
         const btnMenu = document.querySelector('.menu'),
             menu = document.querySelector('menu'),
             closeBtn = document.querySelector('.close-btn'),
             menuItems = menu.querySelectorAll('ul>li');
-            
-            const handlerMenu = () => {
-                menu.classList.toggle('active-menu');
-            };
-            
-           
-             /*const handlerMenu = () => {
-                if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
 
-                    menu.style.transform = `translate(0)`;
-
-                }else {
-
-                    menu.style.transform = `translate(-100%)`;
-                }
-            };*/
-            
-            btnMenu.addEventListener('click', handlerMenu);
-            closeBtn.addEventListener('click', handlerMenu);
-            menuItems.forEach((elem)=>elem.addEventListener('click', handlerMenu));
-
-
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
         };
+
+
+        /*const handlerMenu = () => {
+           if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+
+               menu.style.transform = `translate(0)`;
+
+           }else {
+
+               menu.style.transform = `translate(-100%)`;
+           }
+       };*/
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+
+
+    };
 
     toggleMenu();
 
 
     //popup
 
-    const togglePopUp = ()=> {
+    const togglePopUp = () => {
         const popup = document.querySelector('.popup'),
-        popupContent = popup.querySelector('.popup-content'),
-        popupBtn = document.querySelectorAll('.popup-btn'),
-        popupClose = popup.querySelector('.popup-close');
+            popupContent = popup.querySelector('.popup-content'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = popup.querySelector('.popup-close');
 
 
-        popupBtn.forEach((elem)=>{
+        popupBtn.forEach((elem) => {
 
-            elem.addEventListener( 'click', ()=> {
+            elem.addEventListener('click', () => {
 
-                if (screen.width > 992){
+                if (screen.width > 992) {
 
                     popup.style.display = "block";
                     popupContent.style.left = 0;
                     let count = 0,
                         moveInterval;
 
-                    const popupAnimation = ()=>{ 
-                            moveInterval  = requestAnimationFrame(popupAnimation);
-                            count++ ;
-                                if (count < 50) {
-                                    popupContent.style.left = count + '%';
-                                }else{
-                                    cancelAnimationFrame(moveInterval);
-                                }
+                    const popupAnimation = () => {
+                        moveInterval = requestAnimationFrame(popupAnimation);
+                        count++;
+                        if (count < 38) {
+                            popupContent.style.left = count + '%';
+                        } else {
+                            cancelAnimationFrame(moveInterval);
+                        }
                     };
-                    moveInterval  = requestAnimationFrame(popupAnimation);
-                }else {
+                    moveInterval = requestAnimationFrame(popupAnimation);
+                } else {
                     popup.style.display = "block";
-                } 
+                }
             });
         });
 
-                popupClose.addEventListener('click', ()=>{
-        
-                    popup.style.display = 'none';
-                    popupContent.style.left = 0;
+        popupClose.addEventListener('click', () => {
 
-                });
+            popup.style.display = 'none';
+
+
+        });
 
     };
 
-    togglePopUp ();
+    togglePopUp();
 
 });
